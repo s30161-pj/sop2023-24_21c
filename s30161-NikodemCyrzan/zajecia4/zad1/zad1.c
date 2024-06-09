2a111c396cb97b8d4d9186dd03f1aba8ac66916b
s30161-pj <163664582+s30161-pj@users.noreply.github.com>
2024-06-09 13:07:32 +0200

    Poprawa formatowania zad1.1

--- s30161-NikodemCyrzan/zajecia4/zad1/zad1.c
+++ s30161-NikodemCyrzan/zajecia4/zad1/zad1.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int sum = 0, input = -1;
 
-    while (input != 0)
-    {
+    while (input != 0) {
         scanf("%d", &input);
         sum += input;
     }
