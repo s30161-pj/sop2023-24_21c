a7bf3ea3395f329cd3c252950cd47024cbaea1cf
s30161-pj <163664582+s30161-pj@users.noreply.github.com>
2024-06-09 13:08:34 +0200

    Poprawa formatowania zad1.3

--- s30161-NikodemCyrzan/zajecia4/zad1/zad3.c
+++ s30161-NikodemCyrzan/zajecia4/zad1/zad3.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int i, j, height = atoi(argv[1]), width = 1 + height * 2;
     char *line = malloc(height);
 
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("Podano %d argumentów. Oczekiwano: 1", argc - 1);
         return 0;
     }
 
-    for (i = 0; i < height; i++)
-    {
+    for (i = 0; i < height; i++) {
         for (j = 0; j < width; j++)
             if (j >= width / 2 - i && j <= width / 2 + i)
                 line[j] = '*';
