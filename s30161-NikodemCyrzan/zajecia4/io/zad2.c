158b08d51be8902daa155349120db4bbc837dadc
s30161-pj <163664582+s30161-pj@users.noreply.github.com>
2024-06-09 13:02:56 +0200

    Poprawa formatowania zad2

--- s30161-NikodemCyrzan/zajecia4/io/zad2.c
+++ s30161-NikodemCyrzan/zajecia4/io/zad2.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int i;
     FILE *file = NULL;
     size_t bytesLastRead;
     char *buffer = malloc(16);
     unsigned int readBytes;
 
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("Podano %d argumentów. Oczekiwano 1", argc - 1);
         return 0;
     }
 
     file = fopen(argv[1], "r");
 
-    if (file == NULL)
-    {
+    if (file == NULL) {
         printf("Plik \"%s\" nie istnieje lub nie mógł zostać otworzony", argv[1]);
         return 0;
     }
 
-    while ((bytesLastRead = fread(buffer, 1, 16, file)) > 0)
-    {
+    while ((bytesLastRead = fread(buffer, 1, 16, file)) > 0) {
         printf("%08X  ", readBytes);
 
         for (i = 0; i < bytesLastRead; i++)
@@ -34,8 +30,7 @@ int main(int argc, char **argv)
             printf(" ");
         printf(" ");
 
-        for (i = 0; i < bytesLastRead; i++)
-        {
+        for (i = 0; i < bytesLastRead; i++) {
             unsigned char character = (unsigned char)buffer[i];
             if (character >= 32 && character <= 126)
                 printf("%c", character);
